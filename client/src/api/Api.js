29ec3ca63ee76b3a109d76da4544c55ae17a1982
Nitin <93181605+Nitin9753@users.noreply.github.com>
2024-06-11 00:23:58 +0530

    Update Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api/v1/user' });
+const API = axios.create({ baseURL: 'https://avatart-web-app.onrender.com/api/v1/user' });
 
 
 
